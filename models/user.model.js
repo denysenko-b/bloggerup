09fc5b5d131b8fae99c04b38a7143698eb0c1729
Bohdan <bogdan.05.den@gmail.com>
2021-06-16 21:37:28 +0300

    change project structures

---
+++ models/user.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    userId: {
+        type: Number,
+        unique: true,
+        require: true
+    },
+
+    accountUsername: {
+        type: String,
+        unique: true
+    },
+    accountId: String,
+
+    points: {
+        type: Number,
+        default: 500000
+    },
+
+    referralsCount: {
+        type: Number,
+        default: 0
+    },
+
+    referralParent: {
+        type: Number
+    },
+
+    prevMessage: String,
+
+    notifications: {
+        type: Boolean,
+        default: true
+    }
+})
+
+
+module.exports = mongoose.model('User', UserSchema);
