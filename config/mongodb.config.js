d69db32f56ccc414a88b91bf90ef01a7cfc5d392
Bohdan <bogdan.05.den@gmail.com>
2021-06-16 12:00:24 +0300

    old connection support

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -8,6 +8,6 @@ module.exports = {
         useCreateIndex: true
     },
     get uri() {
-        return `mongodb+srv://root:${this.password}@cluster0.bszr1.mongodb.net/${this.database}?retryWrites=true&w=majority`
+        return `mongodb://root:${this.password}@cluster0-shard-00-00.bszr1.mongodb.net:27017,cluster0-shard-00-01.bszr1.mongodb.net:27017,cluster0-shard-00-02.bszr1.mongodb.net:27017/${this.database }?ssl=true&replicaSet=atlas-vm1dhv-shard-0&authSource=admin&retryWrites=true&w=majority`
     }
 }
