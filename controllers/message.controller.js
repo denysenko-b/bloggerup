64fea67e3bad295d809c39d2d0d91bb602cff23c
Bohdan <bogdan.05.den@gmail.com>
2021-06-17 22:22:10 +0300

    add new message support (create followers task)

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -3,72 +3,80 @@ const TaskController = require('./task.controller');
 
 const validator = require('../validators');
 
+const PointsRate = require('../config/pointsRate.config');
+
 const Texts = require('../texts');
 
 const {
     SelectTaskTypeInlineKeyboard
 } = require('../keyboards');
 
+const keyboardsMessages = Texts.keyboards;
+const replyMessages = Texts.reply.messages;
 
 
 class MessageController {
 
-    constructor() {
+    regNewInstAccount = async (ctx, userId, username, userData) => {
 
-        this.keyboardsMessages = Texts.keyboards;
-        this.replyMessages = Texts.reply.messages;
-    }
+        const replyText = replyMessages.newInstAccount;
+        const messageId = ctx.message.message_id;
+        await ctx.reply(replyText.check);
 
-    regNewInstAccount = async (ctx, userId, username) => {
-        const replyText = this.replyMessages.newInstAccount;
 
-        ctx.reply(replyText.check);
 
-        if (validator.isInstagramUsername(username)) {
-            UserController.updateAccountUsername(userId, username)
-                .then(async (full_name) => {
-                    await UserController.clearPrevMessage(userId);
-                    ctx.reply(replyText.successfulReg(full_name));
-                })
-                .catch(e => {
-                    switch (e) {
-                        case 404:
-                            ctx.reply(replyText.notFound);
-                            break;
+        try {
+            if (!validator.isInstagramUsername(username)) throw 'incorrect';
 
-                        case 'is_private':
-                            ctx.reply(replyText.isPrivate);
-                            break;
+            const full_name = await UserController.updateAccountUsername(userId, username);
+            await UserController.clearPrevMessage(userId);
+            ctx.reply(replyText.successfulReg(full_name || username));
+        } catch (e) {
+            let message = '';
 
-                        case 'already_exists':
-                            ctx.reply(replyText.alreadyExists);
-                            break;
+            switch (e) {
+                case 'incorrect':
+                    message = replyText.incorrect;
+                    break;
 
-                        case 'few_media':
-                            ctx.reply(replyText.fewMedia);
-                            break;
+                case 'not_found':
+                    message = replyText.notFound;
+                    break;
 
-                        case 'few_subscribers':
-                            ctx.reply(replyText.fewSubscribers);
-                            break;
+                case 'is_private':
+                    message = replyText.isPrivate;
+                    break;
 
-                        default:
-                            ctx.reply(this.replyMessages.error())
-                            break;
-                    }
-                })
-        } else {
-            return ctx.reply(replyText.incorrect);
+                case 'already_exists':
+                    message = replyText.alreadyExists;
+                    break;
+
+                case 'few_media':
+                    message = replyText.fewMedia;
+                    break;
+
+                case 'few_subscribers':
+                    message = replyText.fewSubscribers;
+                    break;
+
+                default:
+                    message = replyMessages.error()
+                    break;
+            }
+
+            return ctx.reply(message, {
+                reply_to_message_id: messageId
+            });
         }
     }
 
-    giveTask = async (ctx, userId) => {
-        const replyText = this.replyMessages.tasks.giveTask;
+    giveTask = async (ctx, userId, userData) => {
+        const replyText = replyMessages.tasks.giveTask;
         ctx.reply(replyText.about, SelectTaskTypeInlineKeyboard);
     }
 
-    getAvaliableTasks = async (ctx, userId) => {
-        const replyText = this.replyMessages.tasks.getAvaliableTasks;
+    getAvaliableTasks = async (ctx, userId, userData) => {
+        const replyText = replyMessages.tasks.getAvaliableTasks;
         TaskController.getAvaliableTasks()
             .then(() => {
 
@@ -78,16 +86,16 @@ class MessageController {
             })
     }
 
-    checkBalance = async (ctx, userId) => {
+    checkBalance = async (ctx, userId, {
+        points
+    }) => {
 
-        const replyText = this.replyMessages.tasks.checkBalance;
-
-        const points = await UserController.getPoints(userId);
+        const replyText = replyMessages.tasks.checkBalance;
 
         ctx.reply(replyText.balance(points));
     }
 
-    buyPoints = async (ctx, userId) => {
+    buyPoints = async (ctx, userId, userData) => {
 
     }
 
@@ -103,30 +111,69 @@ class MessageController {
             text
         } = message;
 
-        const prevMessage = await UserController.getPrevMessage(userId);
+        const userData = await UserController.getUserData(userId);
+        const {
+            prevMessage,
+            accountId,
+            points
+        } = userData;
 
+        const {
+            menu
+        } = keyboardsMessages;
 
-        if (prevMessage) {
-            if (prevMessage === 'reg') return this.regNewInstAccount(ctx, userId, text);
-        } else {
-            const {
-                menu
-            } = this.keyboardsMessages;
-
-            for (const command in menu) {
-                if (text.includes(menu[command])) return this[command](ctx, userId);
-            }
+        const reservedMessages = {
+            ...menu
         }
 
-        return ctx.reply(this.replyMessages.iDontUnderstand);
+        for (const command in reservedMessages) {
+            if (text === reservedMessages[command]) {
+
+                if (!accountId) {
+                    await UserController.setPrevMessage(userId, 'reg');
+                    return ctx.reply(replyMessages.newInstAccount.firstCompleteTheRegistration);
+                };
+
+                if (prevMessage) {
+                    await UserController.clearPrevMessage(userId);
+                }
+                return this[command](ctx, userId, userData)
+            };
+        }
+
+        if (prevMessage === 'reg') return this.regNewInstAccount(ctx, userId, text);
+        if (prevMessage === 'points_for_followers') return this.callculateCost(ctx, text, PointsRate.pointsToFollowers, 'followers');
+
+        return ctx.reply(replyMessages.iDontUnderstand);
     }
 
 
+    wait = seconds => new Promise(resolve => setTimeout(() => resolve(), seconds * 1000))
 
+    sendFollowersTaskInfo = async (ctx, userId) => {
+        const replyText = replyMessages.tasks.giveTask.followers;
+        // const messageId = ctx.callbackQuery.message.message_id;
 
+        await ctx.editMessageText(replyText.about, {});
+        await this.wait(3);
+        await ctx.reply(replyText.showCurrentBalance(1000));
+        await this.wait(2);
+        await ctx.reply(replyText.pointsRate);
+        await UserController.setPrevMessage(userId, 'points_for_followers')
+        await this.wait(3);
+        return ctx.reply(replyText.waitFollowersCount);
+    }
 
-    createLikeTask = async (ctx, userId) => {
-        ctx.reply('yes');
+    callculateCost = async (ctx, count, [a, b], taskType) => {
+        if (!validator.isNumber(count)) {
+            return replyText.errors.notANumber;
+        }
+
+        const cost = count * b / a;
+
+        return ctx.reply(replyMessages.tasks.giveTask[taskType].showCost(cost), {
+            reply_to_message_id: ctx.message.message_id
+        });
     }
 
     callbackQueryListener = async ctx => {
@@ -141,9 +188,10 @@ class MessageController {
 
         const {
             giveTask
-        } = this.keyboardsMessages;
+        } = keyboardsMessages;
 
-        if (data === giveTask.likes.callback_data) return this.createLikeTask(ctx, userId);
+        // if (data === giveTask.likes.callback_data) return this.createLikeTask(ctx, userId);
+        if (data === giveTask.followers.callback_data) return this.sendFollowersTaskInfo(ctx, userId);
     }
 }
 
