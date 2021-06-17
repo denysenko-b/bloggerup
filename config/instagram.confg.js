75168e56a035a6d54a8d8ee4a2c2a1fe5b6eeed9
Bohdan <bogdan.05.den@gmail.com>
2021-06-17 22:20:30 +0300

    add some users accound

--- config/instagram.confg.js
+++ config/instagram.confg.js
@@ -1,4 +1,26 @@
-module.exports = {
-    username: 'dodgec42',
-    password: 'qaZxsWbgbfbd143625'
-}
+module.exports = [
+    [
+        'baklazhan43k',
+        '}]9.`beAtKeN7$h'
+    ],
+    [
+        'dodgec42',
+        'ht(_J(mM/NK@55?'
+    ],
+    [
+        'sobaken_iz_patrula',
+        'mUj>zqc(42Uk);v'
+    ],
+    [
+        'nemnozko_ne_ot_suda',
+        ')n<(7Fwh^JtK}`J'
+    ],
+    [
+        'guydurirta',
+        ':Z?!Z9R)k/dp\L:'
+    ],
+    [
+        '',
+        ''
+    ]
+]
