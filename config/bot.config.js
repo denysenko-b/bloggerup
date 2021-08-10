8b9ac622f2fe6a5bd9d0cd3a695973c64d09fd6d
Bohdan <bogdan.05.den@gmail.com>
2021-08-10 20:25:07 +0300

    fix prev bad commit

--- config/bot.config.js
+++ config/bot.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
     token: '1902681377:AAETxRGRs8S4PdrVBMEzS8WECpblLNcOFYM' //1821691095:AAFuO97oXKPpLl8pm2ZPQc_BpGGzg1mLnVY
 }
-=======
-    token: '1886732672:AAGae42eZkVCoHc2rAnCnqjghsX-pHRR7jI'
-}
->>>>>>> 867ee2bc8c659bfd7cf100f88bfa428a6ad8ebe5
